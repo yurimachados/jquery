b60390a7ef10c3331da188768a8406b053c38649
Yuri Machado <ymachado1995@gmail.com>
2023-01-07 13:48:58 -0300

    ...

--- projeto/gulpfile.js
+++ projeto/gulpfile.js
@@ -1,8 +1,22 @@
-const gulp = require('gulp')
-const concat = require("gulp-concat") 
-const cssmin = require("gulp-cssmin")
-const rename = require("gulp-rename")
-const uglify = require("gulp-uglify")
+
+// Dessa maneira tive muita dificuldade, muitos erros.
+
+// const gulp = require('gulp')
+// const concat = require("gulp-concat") 
+// const cssmin = require("gulp-cssmin")
+// const rename = require("gulp-rename")
+// const uglify = require("gulp-uglify")
+// const image = require('gulp-images')
+
+
+// Import que o ECMAScript reconhece.
+
+import gulp from 'gulp';
+import concat from 'gulp-concat';
+import cssmin from 'gulp-cssmin';
+import rename from 'gulp-rename';
+import uglify from 'gulp-uglify';
+import image from 'gulp-images';
 
 
 function tarefasCSS(cb){
@@ -24,5 +38,30 @@ function tarefasJS(){
         .pipe(gulp.dest('./dist/js'))
 }
 
-exports.styles = tarefasCSS
-exports.scripts = tarefasJS
+function tarefasImagem(){
+
+    return gulp.src('./src/images/**')
+        .pipe(image({
+            pngquant: true,
+            optipng: false,
+            zopflipng: true,
+            jpegRecompress: false,
+            mozjpeg: true,
+            gifsicle: true,
+            svgo: true,
+            concurrent: 10,
+            quiet: true
+        }))
+        .pipe(gulp.dest('./dist/images'))
+}
+
+// O ensinado no curso.
+
+// exports.styles = tarefasCSS
+// exports.scripts = tarefasJS
+// exports.images = tarefasImagem
+
+// Esse é o export que foi possivel com o ECMAScript.
+export const styles = tarefasCSS;
+export const scripts = tarefasJS;
+export const images = tarefasImagem;
