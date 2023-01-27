dbd846ddbfcbda8ad3e8eba8d121a245955466ed
Yuri Machado <ymachado1995@gmail.com>
2023-01-27 14:11:05 -0300

    Babel

--- projeto/gulpfile.js
+++ projeto/gulpfile.js
@@ -12,49 +12,59 @@
 // Import que o ECMAScript reconhece.
 
 import gulp from 'gulp';
+const { series } = gulp
 import concat from 'gulp-concat';
 import cssmin from 'gulp-cssmin';
 import rename from 'gulp-rename';
 import uglify from 'gulp-uglify';
 import imagemin from 'gulp-imagemin';
+import htmlmin from 'gulp-htmlmin';
+import babel from 'gulp-babel';
+
 
 
 function tarefasCSS(cb) {
 
-    return gulp.src([
+    gulp.src([
         './node_modules/bootstrap/dist/css/bootstrap.css',
         './vendor/owl/css/owl.css',
         './node_modules/@fortawesome/fontawesome-free/css/fontawesome.css',
         './vendor/jquery-ui-ligth/jquery-ui.min.css',
         './vendor/jquery-ui-ligth/jquery-ui.css',
         './src/css/style.css'
-    ])
-        .pipe(concat('styles.css'))
+    ])  .pipe(concat('styles.css'))
         .pipe(cssmin())
         .pipe(rename({ suffix: '.min' })) // libs.min.css
         .pipe(gulp.dest('./dist/css'))
 
+    return cb()
 }
 
-function tarefasJS() {
+function tarefasJS(cb) {
 
-    return gulp.src([
+    gulp.src([
         './node_modules/jquery/dist/jquery.js',
         './node_modules/bootstrap/dist/js/bootstrap.js',
         './vendor/owl/js/owl.js',
         './vendor/jquery-mask/dist/jquery.mask.js',
         './vendor/jquery-ui-ligth/jquery-ui.js',
-        './src/js/custom.js'
-    ])
+        './src/js/custom.js',
+        './src/js/validations.js'
+    ])  .pipe(babel({
+        comments: true,
+        presets: ['@babel/env']
+    }))
         .pipe(concat('scripts.js'))
         .pipe(uglify())
         .pipe(rename({ suffix: '.min' })) // libs.min.js
         .pipe(gulp.dest('./dist/js'))
+
+    return cb();
 }
 
-function tarefasImagem() {
+function tarefasImagem(cb) {
 
-    return gulp.src('./src/images/*')
+    gulp.src('./src/images/*')
         .pipe(imagemin({
             pngquant: true,
             optipng: false,
@@ -67,6 +77,18 @@ function tarefasImagem() {
             quiet: true
         }))
         .pipe(gulp.dest('./dist/images'))
+    
+    return cb()
+}
+
+// POC
+function tarefasHTML(callback){
+
+    gulp.src('./src/**/*.html')
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest('./dist/'))
+
+    return callback();
 }
 
 // O ensinado no curso.
@@ -76,6 +98,8 @@ function tarefasImagem() {
 // exports.images = tarefasImagem
 
 // Esse é o export que foi possivel com o ECMAScript.
+export default series(tarefasHTML, tarefasJS, tarefasCSS) 
+
 export const styles = tarefasCSS;
 export const scripts = tarefasJS;
 export const images = tarefasImagem;
